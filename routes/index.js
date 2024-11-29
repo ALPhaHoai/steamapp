79d2a61eec211b12028c80499f4f182f8ec34c26
Drake <drake@eup.net.vn>
2024-11-29 10:17:27 +0700

    commit

--- routes/index.js
+++ routes/index.js
@@ -5,24 +5,25 @@ const router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.send("steam app hello world!");
+    res.send("steam app hello world!");
 });
 
 router.get("/getGameNameByAppIds", async function (req, res, next) {
-  const appIds = req.query.appIds?.split(',')?.map(parseInt).filter(Boolean);
+    const appIds = req.query.appIds?.split(',')?.map(parseInt).filter(Boolean);
 
-  res.json({
-    results: await collection.SteamApp.find({
-      appId: {
-        $in: appIds,
-      },
-    })
-        .project({
-          appId: 1,
-          name: 1,
+    res.json({
+        results: await collection.SteamApp.find({
+            appId: {
+                $in: appIds,
+            },
         })
-        .toArray()
-  })
+            .project({
+                _id: 0,
+                appId: 1,
+                name: 1,
+            })
+            .toArray()
+    })
 });
 
 export default router;
